d518d99a165356b908bc5789a5f91ae1bc1ea97e
novembertwotwo <korea621214@naver.com>
2025-01-20 14:10:29 +0900

    BOJ2587.java

---
+++ src/정렬/BOJ2587.java
@@ -0,0 +1,25 @@
+package 정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class BOJ2587 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] array = new int[5];
+        int sum = 0;
+        for(int i = 0 ;i<5;i++){
+            int num = Integer.parseInt(br.readLine());
+            array[i] = num;
+            sum += num;
+        }
+        Arrays.sort(array);
+        StringBuilder result = new StringBuilder();
+        result.append(sum / 5).append("\n");
+        result.append(array[2]);
+        System.out.println(result);
+    }
+}
